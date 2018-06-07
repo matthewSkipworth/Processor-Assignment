99ed0843639d704c6963e0a2391c804a41fc5651
Matthew Skipworth <mfskipwo@uw.edu>
2018-06-07 13:26:50 -0700

    comments added

--- Processor/ALU.sv
+++ Processor/ALU.sv
@@ -1,30 +1,35 @@
-module ALU(A, B, Sel, Q);
+//Bruce Baker and Matthew Skipworth
+//ALU module
 
+//module signature
+module ALU(A, B, Sel, Q);
+//input/output assignments
 	input [2:0] Sel;
 	input [15:0] A, B;
 	
 	output logic [15:0] Q;
-	
+		
+//This switch statement implements the ALU without the explicit use of user
+//implemented adders. This allows us to get exactly what we need while
+//introducing minimal error.
 	always_comb begin
-	
-	case(Sel) 
-		
-		0: Q = 0;
-		1: Q = A + B;
-		2: Q = A - B;
-		3: Q = A;
-		4: Q = A ^ B;
-		5: Q = A | B;
-		6: Q = A & B;
-		7: Q = A + 1'b1;
-		
-		default: Q = 0;
-	endcase
-	
+		case(Sel) 
+			
+			0: Q = 0;
+			1: Q = A + B;
+			2: Q = A - B;
+			3: Q = A;
+			4: Q = A ^ B;
+			5: Q = A | B;
+			6: Q = A & B;
+			7: Q = A + 1'b1;
+			
+			default: Q = 0;
+		endcase
 	end
 	
 endmodule	
-
+//The testbench for the mo
 module ALU_testbench;
 
 	logic [2:0] Sel;
--- Processor/Data_Path.sv
+++ Processor/Data_Path.sv
@@ -1,3 +1,7 @@
+//Bruce Baker & Matthew Skipworth
+//Datapath supermodule
+
+//module signature
 module Data_Path(input [7:0] D_addr,
 				input Clk,
 				input Rst,
@@ -12,69 +16,26 @@ module Data_Path(input [7:0] D_addr,
 				output [15:0] ALU_A,
 				output [15:0] ALU_B);
 				
-	//logic [15:0] data;
+//input/output assignments		
 	logic [15:0] q;
 	logic [15:0] Q;
 	logic [15:0] Mux_result;
 	logic [15:0] Ra_Data;
 	logic [15:0] Rb_Data;
 
+//Data Memory instantiation
 	 Data_Memory DM(D_addr, Clk, Ra_Data, D_wr, q);
 	
+//2 to 1 mux instantiation	
 	 Mux_16_Bit_2_to_1 MUX(q, Q, MuxSel, Mux_result);
 	
+//ALU instantiation	
 	 ALU ALU(Ra_Data, Rb_Data, ALU_s, Q);
-	
+
+//Register File instantiation	 
 	RegisterFile RF(  Clk, Rst, RFWen, RFWAddr, Mux_result, RF_A_Addr, RF_B_Addr, 1'b1, 1'b1, Ra_Data, Rb_Data );
 
 	assign ALU_Out = Q;
 	assign ALU_A = Ra_Data;
 	assign ALU_B = Rb_Data;
 endmodule
-/*
-module Data_Path_testbench;
-
-
-	logic [7:0] D_addr;
-	logic Clk, Rst, D_wr, MuxSel, RFWen;
-	logic [15:0] ALU_Out, ALU_A, ALU_B;
-	logic [3:0] RF_A_Addr, RF_B_Addr, RFWAddr;
-	logic [2:0] ALU_s;
-	
-	integer i;
-	
-	Data_Path dut(D_addr,
-				 Clk,
-				 Rst,
-				 D_wr,
-				 MuxSel,
-				 ALU_s,
-				 RF_A_Addr,
-				 RF_B_Addr,
-				 RFWen,
-				 RFWAddr,
-				 ALU_Out,
-				 ALU_A,
-				 ALU_B);
-				 
-				 
-	initial begin
-		D_addr = 8'b00110011;
-		Rst = 0;
-		D_wr = 1;
-		MuxSel = 0;
-		ALU_s = 3'b000; 
-		RF_A_Addr = 4'b0000;
-		RF_B_Addr = 4'b0001;
-		RFWen = 0;
-		RFWAddr = 4'b0000; 
-		#10;
-		
-		for (int i = 0; i < 8; i++) begin
-		ALU_s = i;
-			$display(ALU_Out, ALU_A, ALU_B);
-		end
-		
-	end
-				
-endmodule		*/	
